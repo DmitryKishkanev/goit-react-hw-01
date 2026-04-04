19890bd92d4f5990327e3f966b0bfb73dd4b870e
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-04-04 16:36:37 +0300

    добавил div

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import style from '@/components/App/App.module.css';
 export default function App() {
   return (
     <div className={style.app}>
+      <div></div>
       <Profile
         name={userData.username}
         tag={userData.tag}
